a6f542ff7bef5baac6ab272d4662d492a3a2c0e1
Jovan Tomic <jtomic1@gmail.com>
2022-12-01 14:22:45 +0100

    Message service created

--- nwt-kts-ui/src/app/shared/shared.module.ts
+++ nwt-kts-ui/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialModule } from './material/material/material.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessageComponent } from './services/message-service/message-service.service';
 
 @NgModule({
-  declarations: [NavbarComponent],
-  imports: [CommonModule, MaterialModule],
+  declarations: [NavbarComponent, MessageComponent],
+  imports: [CommonModule, MaterialModule, FontAwesomeModule],
   exports: [MaterialModule, NavbarComponent],
 })
 export class SharedModule {}

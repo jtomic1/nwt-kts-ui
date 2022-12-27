c6bf39d86c7cd92316c15aafc853f8933f48ee6b
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2022-12-27 18:25:28 +0100

    Added search and waypoints

--- nwt-kts-ui/src/app/features/clientpage/components/clientpage-map/clientpage-map.component.ts
+++ nwt-kts-ui/src/app/features/clientpage/components/clientpage-map/clientpage-map.component.ts
@@ -87,6 +87,19 @@ export class ClientpageMapComponent implements AfterViewInit, OnDestroy {
                            .bindPopup('Vozilo je zauzeto.');
     
     markerTaxiTaken.addTo(this.map);
+
+    this.map.on('click',  (e: {latlng: any}) => {
+      if (!this.isStartSet) {
+        this.setStartingMarker(e.latlng, true);
+      } else if (!this.isDestinationSet) {
+        this.setDestinationMarker(e.latlng, true);
+
+        this.map.removeLayer(this.startMarker);
+        this.map.removeLayer(this.destinationMarker);
+
+        this.makeRoute();
+      }
+    });
   }
 
   createFormGroup(): FormGroup {
@@ -99,27 +112,225 @@ export class ClientpageMapComponent implements AfterViewInit, OnDestroy {
   }
 
   onStartSearch(): void {
-
+    this.mapService.getCoordinates(this.form.controls['start'].value + ', Novi Sad').subscribe(
+      (result: any) => {
+        console.log(result);        
+        if (result.features.length !== 0) {
+          if (!this.isStartSet) {
+            var latlng = {lat: result.features[0].geometry.coordinates[1],
+                          lng: result.features[0].geometry.coordinates[0]};
+            this.setStartingMarker(latlng , false);
+          } else {
+            var destination = [this.route.getWaypoints()[this.route.getWaypoints().length - 1].latLng.lat, this.route.getWaypoints()[this.route.getWaypoints().length - 1].latLng.lng]
+            this.route.setWaypoints([
+            L.latLng([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]]),
+            L.latLng([destination[0], destination[1]])
+          ]);
+          }
+        } else {
+          alert('Pretraga neuspesna!');
+        }
+      }    
+    );
   }
 
   onDestinationSearch(): void {
+    this.mapService.getCoordinates(this.form.controls['destination'].value + ', Novi Sad').subscribe(
+      (result: any) => {
+        if (result.features.length !== 0) {
+          if (!this.isDestinationSet) {
+            var latlng = {lat: result.features[0].geometry.coordinates[1],
+                          lng: result.features[0].geometry.coordinates[0]};
+            this.setDestinationMarker(latlng, false);
+  
+            this.map.removeLayer(this.startMarker);
+            this.map.removeLayer(this.destinationMarker);
+  
+            this.makeRoute();
+          } else {
+            var start = [this.route.getWaypoints()[0].latLng.lat, this.route.getWaypoints()[0].latLng.lng]
+            this.route.setWaypoints([
+              L.latLng([start[0], start[1]]),
+              L.latLng([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]])
+            ]);
+          }
+        } else {
+          alert('Pretraga neuspesna!')
+        }
+      }
+    );
+  }
 
+  setStartingMarker(latlng: any, setAddress: boolean): void {
+    this.startMarker = L.marker([latlng.lat, latlng.lng], {draggable: true});
+    this.startMarker.addTo(this.map);
+    this.isStartSet = true;
+      
+    if (setAddress) {
+      this.mapService.getAddress(latlng).subscribe(
+        (result: any) => {          
+          var data = result.features[0].properties.geocoding;
+          this.setStartFormControl(data);
+        }
+      );
+    }
+  }
+
+  setDestinationMarker(latlng: any, setAddress: boolean): void {
+    this.destinationMarker = L.marker([latlng.lat, latlng.lng], {draggable: true});
+    this.destinationMarker.addTo(this.map);
+    this.isDestinationSet = true;
+        
+    if (setAddress) {
+      this.mapService.getAddress(latlng).subscribe(
+        (result: any) => {
+          //console.log(result);
+          var data = result.features[0].properties.geocoding;
+          this.setDestinationFormControl(data);
+        }
+      );
+    }
+  }
+
+  makeRoute(): void {
+    this.route = L.Routing.control({
+      waypoints: [
+        L.latLng(this.startMarker.getLatLng()),
+        L.latLng(this.destinationMarker.getLatLng())
+      ],            
+      showAlternatives: true,
+      altLineOptions: {
+        styles: [
+          {color: 'black', opacity: 0.15, weight: 9},
+          {color: 'white', opacity: 0.8, weight: 6},
+          {color: 'blue', opacity: 0.5, weight: 2}
+        ],
+        extendToWaypoints: false,
+        missingRouteTolerance: 0
+      }
+    }).on('routesfound',  (e) => {
+      //console.log(e);
+      this.mapService.getAddress(e.waypoints[0].latLng).subscribe(
+        (result: any) => {
+          //console.log(result);
+          var data = result.features[0].properties.geocoding;
+          this.setStartFormControl(data);
+        }
+      );
+      this.mapService.getAddress(e.waypoints[e.waypoints.length - 1].latLng).subscribe(
+        (result: any) => {
+          var data = result.features[0].properties.geocoding;
+          this.setDestinationFormControl(data);
+        }        
+      );
+      this.form.controls['time'].setValue(Math.round(e.routes[0].summary.totalTime / 60) + ' minuta');
+      this.form.controls['price'].setValue(Math.round(250 + (e.routes[0].summary.totalDistance / 1000)*120) + ' dinara');
+    })
+    .on('routeselected', (e) => {
+      this.form.controls['time'].setValue(Math.round(e.route.summary.totalTime / 60) + ' minuta');
+      this.form.controls['price'].setValue(Math.round(250 + (e.route.summary.totalDistance / 1000)*120) + ' dinara');
+    })
+    .on('waypointschanged',  (e) => {
+      console.log('waypointchanged');
+      console.log(e);
+      this.addStationWithMarkerDrag(e.waypoints);
+    })
+    .addTo(this.map);
+
+    this.route.hide();
   }
 
   removeStation(station: OnWayStation): void {
-
+    const index = this.onWayStations.indexOf(station);
+    if (index >= 0) {
+      this.onWayStations.splice(index, 1);      
+      var waypoints: L.Routing.Waypoint[] = this.route.getWaypoints();
+      for (let i = 0; i < waypoints.length; i++) {
+        if (station.lat === waypoints[i].latLng.lat && station.lng === waypoints[i].latLng.lng) {
+          this.route.spliceWaypoints(i, 1);
+        }
+      }
+    }
   }
 
   addStation(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    this.mapService.getCoordinates((event.value).trim() + ', Novi Sad').subscribe(
+      (result: any) => {
+        if (result.features.length !== 0 && this.isStartSet && this.isDestinationSet) {                             
+          var station: OnWayStation = {address: value, lat: result.features[0].geometry.coordinates[1],
+                                        lng: result.features[0].geometry.coordinates[0]};
+          this.onWayStations.push(station);  
+          this.route.spliceWaypoints(this.route.getWaypoints().length - 1, 0, L.latLng([station.lat, station.lng]));    
+        } 
+        else if (!this.isStartSet || !this.isDestinationSet) {
+          alert('Prvo unesite polaziste i krajnju lokaciju');
+        } else {
+          alert('Neuspesno dodavanje medjustanica!');
+        }
+      }    
+    );
     
+    event.chipInput!.clear();
   }
 
   removeEmail(email: string): void {
+    const index = this.splitFare.indexOf(email);
 
+    if (index >= 0) {
+      this.splitFare.splice(index, 1);
+    }
   }
 
   addEmail(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
 
+    if (value) {
+      this.splitFare.push(value);
+    }
+
+    event.chipInput!.clear();
+  }
+
+  setStartFormControl(data: any): void {
+    if (data.housenumber !== undefined && data.street !== undefined) {
+      this.form.controls['start'].setValue(data.street + ' ' + data.housenumber);
+    }            
+    else if (data.housenumber === undefined && data.street !== undefined) {
+      this.form.controls['start'].setValue(data.street);
+    } else {
+      this.form.controls['start'].setValue(data.label);
+    }
+  }
+
+  setDestinationFormControl(data: any): void {
+    if (data.housenumber !== undefined && data.street !== undefined) {
+      this.form.controls['destination'].setValue(data.street + ' ' + data.housenumber);
+    }            
+    else if (data.housenumber === undefined && data.street !== undefined) {
+      this.form.controls['destination'].setValue(data.street);
+    } else {
+      this.form.controls['destination'].setValue(data.label);
+    }
+  }
+
+  addStationWithMarkerDrag(waypoints: any) {
+    this.onWayStations = [];
+    for (let i = 1; i < waypoints.length - 1; i++) {
+      this.mapService.getAddress(waypoints[i].latLng).subscribe(
+        (result: any) => {
+          var data = result.features[0].properties.geocoding;
+          if (data.housenumber !== undefined && data.street !== undefined) {
+            var station: OnWayStation = {address: data.street + ' ' + data.housenumber, lat: waypoints[i].latLng.lat, lng: waypoints[i].latLng.lng};
+            this.onWayStations.push(station);
+          } 
+          else {
+            var station: OnWayStation = {address: 'NN ' + i, lat: waypoints[i].latLng.lat, lng: waypoints[i].latLng.lng};
+            this.onWayStations.push(station);
+          }
+        }
+      );
+    }
   }
 
 }

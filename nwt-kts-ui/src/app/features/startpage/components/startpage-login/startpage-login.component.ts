7fc0cf29cd84837d8bb9ad251e6dd4a02a619deb
GajicMladen <mladengajic00@gmail.com>
2022-12-29 02:47:32 +0100

    .

--- nwt-kts-ui/src/app/features/startpage/components/startpage-login/startpage-login.component.ts
+++ nwt-kts-ui/src/app/features/startpage/components/startpage-login/startpage-login.component.ts
@@ -45,7 +45,6 @@ export class StartpageLoginComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (res: any) => {
-          res = JSON.parse(res);
           this.loginService.setUserData(res);
         },
         error: (err) => {
